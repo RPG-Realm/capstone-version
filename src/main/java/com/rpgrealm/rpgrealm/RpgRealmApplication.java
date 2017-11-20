152e505abf6d7f6538360c03cc770e05a5d19a50
chasehammock <chasehammock@gmail.com>
2017-11-20 11:16:22 -0600

    Initial commit

---
+++ src/main/java/com/rpgrealm/rpgrealm/RpgRealmApplication.java
@@ -0,0 +1,12 @@
+package com.rpgrealm.rpgrealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgRealmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgRealmApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rpgrealm/rpgrealm/RpgRealmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rpgrealm.rpgrealm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RpgRealmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
