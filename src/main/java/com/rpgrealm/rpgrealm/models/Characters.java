419f10caa5dd4e6cc6838480618da202a82001ff
chasehammock <chasehammock@gmail.com>
2017-11-21 10:41:12 -0600

    added characters and games model

---
+++ src/main/java/com/rpgrealm/rpgrealm/models/Characters.java
@@ -0,0 +1,67 @@
+package com.rpgrealm.rpgrealm.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "characters")
+public class Characters {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String character_name;
+
+  @Column
+  private String character_type;
+
+  @Column
+  private Long character_image_id;
+
+  @Column
+  private Long character_file_id;
+
+  public Characters() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getCharacter_name() {
+    return character_name;
+  }
+
+  public void setCharacter_name(String character_name) {
+    this.character_name = character_name;
+  }
+
+  public String getCharacter_type() {
+    return character_type;
+  }
+
+  public void setCharacter_type(String character_type) {
+    this.character_type = character_type;
+  }
+
+  public Long getCharacter_image_id() {
+    return character_image_id;
+  }
+
+  public void setCharacter_image_id(Long character_image_id) {
+    this.character_image_id = character_image_id;
+  }
+
+  public Long getCharacter_file_id() {
+    return character_file_id;
+  }
+
+  public void setCharacter_file_id(Long character_file_id) {
+    this.character_file_id = character_file_id;
+  }
+}
