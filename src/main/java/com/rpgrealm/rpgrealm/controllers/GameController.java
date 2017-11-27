7603bd63b6230e0304bfd1404a1876d1e4622c19
chasehammock <chasehammock@gmail.com>
2017-11-27 13:55:32 -0600

    consolidated game controllers into one

--- src/main/java/com/rpgrealm/rpgrealm/controllers/GameController.java
+++ src/main/java/com/rpgrealm/rpgrealm/controllers/GameController.java
@@ -7,9 +7,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CreateGameController {
+public class GameController {
+
   @GetMapping("/create_game")
-  public String showCreateGame() {
+  public String createGame() {
     return "create_game";
   }
+
+  @GetMapping("/view_game")
+  public String viewGame() {
+    return "view_game";
+  }
+
+  @GetMapping("/edit_game")
+  public String editGame() {
+    return "create_game";
+  }
+
+  @GetMapping("/games_mapped")
+  public String mappedGame() {
+    return "games_mapped";
+  }
+
 }
