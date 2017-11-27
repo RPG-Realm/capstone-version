3b8d4d498a3609849fc467370adc4f3aee8b9939
Erik <erikrhansen@ricealumni.net>
2017-11-27 13:18:55 -0600

    Working on our temporary log in solution. Security flaw in password verification will be fixed when we implement authentication later this week.

--- src/main/java/com/rpgrealm/rpgrealm/controllers/AuthenticationController.java
+++ src/main/java/com/rpgrealm/rpgrealm/controllers/AuthenticationController.java
@@ -3,13 +3,37 @@
  */
 package com.rpgrealm.rpgrealm.controllers;
 
+import com.rpgrealm.rpgrealm.models.User;
+import com.rpgrealm.rpgrealm.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
+    private final UserRepository usrRep;
+
+    public AuthenticationController(UserRepository usrRep){
+        this.usrRep=usrRep;
+    }
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(Model model) {
+        model.addAttribute("user", new User());
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user){
+        boolean userExists=usrRep.findByUsername(user.getUsername())!=null;
+        System.out.println(userExists);
+        boolean passwordMatch=user.getPassword().equalsIgnoreCase(usrRep.findByUsername(user.getUsername()).getPassword());
+        System.out.println(passwordMatch);
+        if(userExists&&passwordMatch){
+            return "redirect:/profile";
+        }else{
+            return "/login";
+        }
     }
 }
