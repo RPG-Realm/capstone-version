1dad3f319ac9e630067370d27c0d2856952876f9
Erik <erikrhansen@ricealumni.net>
2017-11-28 10:45:08 -0600

    can view and edit a user profile. Currently userprofile is hardcoded

--- src/main/java/com/rpgrealm/rpgrealm/controllers/UserController.java
+++ src/main/java/com/rpgrealm/rpgrealm/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -25,8 +26,14 @@ public class UserController {
   }
 
   @GetMapping("/user-profile/edit/{id}")
-  public String editUser(@PathVariable Long id, User user) {
-
+  public String editUser(@PathVariable Long id, Model model) {
+    model.addAttribute("user", usrRep.findOne(id));
     return "user-profile-edit";
+  }
+
+  @PostMapping("/user-profile/edit/{id}")
+  public String commitEditUser(@PathVariable Long id, User user){
+    usrRep.save(user);
+    return "user-profile";
   }
 }
