675b8c772982a2975ffa879cb9636f6a548c30ed
Erik <erikrhansen@ricealumni.net>
2017-11-28 13:10:27 -0600

    pushing up current work so chase can get db structure

--- src/main/java/com/rpgrealm/rpgrealm/repositories/CharacterRepository.java
+++ src/main/java/com/rpgrealm/rpgrealm/repositories/CharacterRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CharacterRepository extends CrudRepository<Character, String> {
+public interface CharacterRepository extends CrudRepository<Character, Long> {
   // empty interface body
   // we can define custom methods here, but for the exercise, all we need
   // are the methods that come from CrudRepository
