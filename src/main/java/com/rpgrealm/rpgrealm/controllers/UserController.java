13679a5249862be64c6a83b4bd39eb85fca72745
Erik <erikrhansen@ricealumni.net>
2017-11-28 09:42:18 -0600

    starting to add the logic to show a unique users profile as well as characters tied to him.

--- src/main/java/com/rpgrealm/rpgrealm/controllers/UserController.java
+++ src/main/java/com/rpgrealm/rpgrealm/controllers/UserController.java
@@ -3,18 +3,30 @@
  */
 package com.rpgrealm.rpgrealm.controllers;
 
+import com.rpgrealm.rpgrealm.models.User;
+import com.rpgrealm.rpgrealm.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
+  private final UserRepository usrRep;
+
+  public UserController(UserRepository usrRep){
+    this.usrRep=usrRep;
+  }
+
   @GetMapping("/user-profile")
-  public String showUser() {
+  public String showUser(Model model) {
+    model.addAttribute("user", usrRep.findOne(1L));
     return "user-profile";
   }
 
-  @GetMapping("/user-profile/edit")
-  public String editUser() {
+  @GetMapping("/user-profile/edit/{id}")
+  public String editUser(@PathVariable Long id, User user) {
+
     return "user-profile-edit";
   }
 }
