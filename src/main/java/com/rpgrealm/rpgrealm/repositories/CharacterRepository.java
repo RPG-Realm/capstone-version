cffc4c7febdb0d28db4a277a2c3f1f25e3a79bbd
Erik <erikrhansen@ricealumni.net>
2017-12-01 12:54:06 -0600

    fixed the mySQL to view a list of characters from the profile page.

--- src/main/java/com/rpgrealm/rpgrealm/repositories/CharacterRepository.java
+++ src/main/java/com/rpgrealm/rpgrealm/repositories/CharacterRepository.java
@@ -13,8 +13,13 @@ public interface CharacterRepository extends CrudRepository<Character, Long> {
   // we can define custom methods here, but for the exercise, all we need
   // are the methods that come from CrudRepository
 
+//    @Query( nativeQuery = true,
+//            value=
+//            "SELECT * FROM characters WHERE user_id =?1")
+//    List<Character> findByUserId(Long user_id);
+
     @Query( nativeQuery = true,
-            value=
-            "SELECT * FROM characters WHERE user_id =?1")
+            value =
+            "SELECT * FROM characters c JOIN app_files af on af.character_id = c.id WHERE af.user_id = ?;")
     List<Character> findByUserId(Long user_id);
 }
