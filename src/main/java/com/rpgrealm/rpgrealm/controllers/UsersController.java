f523297888268d7b9030fa64d9efeaf42c03fd32
Erik <erikrhansen@ricealumni.net>
2017-12-02 21:45:34 -0600

    images are half sticky. If they already exist editing their related files won't wipe them out.

--- src/main/java/com/rpgrealm/rpgrealm/controllers/UsersController.java
+++ src/main/java/com/rpgrealm/rpgrealm/controllers/UsersController.java
@@ -12,10 +12,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UsersController {
@@ -66,6 +64,6 @@ public class UsersController {
     String hash = passwordEncoder.encode(user.getPassword());
     user.setPassword(hash);
     usrRep.save(user);
-    return "user-profile";
+    return "/user-profile";
   }
 }
