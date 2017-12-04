348b94d0d0359bb5a7b10d1df4fd23b0a246f7c5
Erik <erikrhansen@ricealumni.net>
2017-12-03 23:18:22 -0600

    Added a try catch into home controller to link to profile page and game search if the user is logged in to session.

--- src/main/java/com/rpgrealm/rpgrealm/controllers/HomeController.java
+++ src/main/java/com/rpgrealm/rpgrealm/controllers/HomeController.java
@@ -3,13 +3,32 @@
  */
 package com.rpgrealm.rpgrealm.controllers;
 
+import com.rpgrealm.rpgrealm.models.User;
+import com.rpgrealm.rpgrealm.repositories.Users;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final Users usrRep;
+
+    public HomeController(Users usrRep){
+        this.usrRep=usrRep;
+    }
     @GetMapping("/home")
-    public String showHomePage() {
-        return "home";
+    public String showHomePage(Model model) {
+        try{
+        User user=(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User dbUser=usrRep.findOne(user.getId());
+            model.addAttribute("user",dbUser);
+            return "home";
+        }catch (Exception e){
+            return "home";
+        }
+
+
+
     }
 }
